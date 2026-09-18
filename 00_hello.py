16ddfbadcd099fa25e09a64a2acabdd24f8901e2
arklay <hernandezvelisl@gmail.com>
2026-09-17 21:15:04 -0600

    primera clase python

---
+++ 00_hello.py
@@ -0,0 +1,24 @@
+#Esto es un comentario: Hola mundo con Python
+#Nuestro Hola Mundo en Python
+print("Hola Python")
+
+
+"""
+Este es un comentario que funciona en muchísimas líneas :)
+porque es muy divertido hacer lo que te gusta 
+y seguir tus sueños
+"""
+
+'''
+Incluso con 
+comillas simples 
+se puede comentar en varias lineas
+'''
+#Como saber el tipo de dato que estamos utilizando
+print(type("Soy un datos Str")) #Tipo de dato 'str'
+print(type(10)) #Tipo de dato 'int'
+print(type(10.5)) #Tipo de dato 'float'
+print(type(1 + 1j)) #Tipo de dato 'complex'
+print(type(True)) #Tipo de dato 'bool'
+
+
---
+++ 01_variables.py
@@ -0,0 +1,4 @@
+#Variables en Python
+
+my_variable = "My String Variable"
+print(my_variable)
